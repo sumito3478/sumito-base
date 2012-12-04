dc98605505f0a462b9b0061d59f981dc3dc21395
sumito3478 <sumito3478@gmail.com>
2012-12-05 03:14:19 +0900

    Add spec for Breaks

---
+++ src/test/scala/sumito3478/util/control/BreaksSpec.scala
@@ -0,0 +1,53 @@
+package sumito3478.util.control
+
+import org.specs2.mutable.SpecificationWithJUnit
+import org.specs2.specification.Scope
+import scala.collection.mutable.Queue
+
+class BreaksSpec extends SpecificationWithJUnit {
+  "Breaks#breakable" should {
+    "be able to break from scope" in {
+      val queue = new Queue[Int]
+      Breaks.breakable {
+        break =>
+          0 to 10 foreach {
+            i =>
+              if(i < 5) {
+                queue.enqueue(i)
+              } else {
+                break
+              }
+          }
+      }
+      val ret = queue.toArray
+      ret mustEqual Array(0, 1, 2, 3, 4)
+    }
+    
+    "be able to break from nested breakable scope" in {
+      val queue = new Queue[Int]
+      Breaks.breakable {
+        break =>
+          0 to 10 foreach {
+            i =>
+              if(i < 5) {
+                queue.enqueue(i)
+              } else {
+                Breaks.breakable {
+                  break2 =>
+                    11 to 20 foreach {
+                      i =>
+                        if(i < 15) {
+                          queue.enqueue(i)
+                        } else {
+                          break // break from first breakable, not second
+                        }
+                    }
+                }
+              }
+          }
+      }
+      val ret = queue.toArray
+      ret mustEqual Array(0, 1, 2, 3, 4, 11, 12, 13, 14)
+    }
+  }
+}
