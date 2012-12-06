232370d5b826e014384bfac33592f92f91cc5a21
sumito3478 <sumito3478@gmail.com>
2012-12-07 08:58:19 +0900

    Add settings for publishing

--- project/Build.scala
+++ project/Build.scala
@@ -12,6 +12,16 @@ object SumitoTextBuild extends Build {
     Seq(
       version := "0.0.1",
       scalaVersion := "2.10.0-RC3",
+      publishMavenStyle := true,
+      publishTo := Some(Resolver.sftp(
+        "sumito3478 Maven Repository",
+        "maven.sumito3478.info",
+        46877,
+        "/var/www/maven.sumito3478.info") as (
+        "sumito3478",
+        new java.io.File(
+          new java.io.File(System.getProperty("user.home")),
+          ".ssh/sumito3478-sshkey"))),
       libraryDependencies ++= Seq(
         "org.specs2" %% "specs2" % "1.12.3" % "test",
         "org.mockito" % "mockito-core" % "1.9.5" % "test",
