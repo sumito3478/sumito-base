604194f5ea0c2ccc959c9aa44d94fe868f0bddf6
sumito3478 <sumito3478@gmail.com>
2012-12-07 14:13:29 +0900

    Modify trait RichIterator to be implicit class

--- src/main/scala/sumito3478/collection/package.scala
+++ src/main/scala/sumito3478/collection/package.scala
@@ -1,6 +1,47 @@
 package sumito3478
 
+import scala.collection.immutable.VectorBuilder
+import sumito3478.util.control.Breaks.breakable
+
 package object collection {
-  implicit class ImplicitRichIterator[+A](
-    val intern: Iterator[A]) extends RichIterator[A]
+  implicit class RichIterator[+A](
+      val intern: Iterator[A]) extends WrapperLike[Iterator[A]] {
+    def forceTake(n: Int): IndexedSeq[A] = {
+      val builder = new VectorBuilder[A]
+      breakable {
+        break =>
+          0 until n foreach {
+            _ =>
+              if (intern.hasNext) {
+                builder += intern.next
+              } else {
+                break
+              }
+          }
+      }
+      builder.result
+    }
+
+    def forceDrop(n: Int): this.type = {
+      breakable {
+        break =>
+          0 until n foreach {
+            _ =>
+              if (intern.hasNext) {
+                intern.next
+              } else {
+                break
+              }
+          }
+      }
+      this
+    }
+
+    def lookAhead: LookAheadIterator[A] = {
+      val i = intern
+      new LookAheadIterator[A] {
+        val intern: Iterator[A] = i
+      }
+    }
+  }
 }
