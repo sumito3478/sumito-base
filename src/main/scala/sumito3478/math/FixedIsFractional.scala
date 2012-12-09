dfb799b045a6d1b7bec7cb6d05ab7a03de59e203
sumito3478 <sumito3478@gmail.com>
2012-12-10 02:04:21 +0900

    Add FixedIsFractional

---
+++ src/main/scala/sumito3478/math/FixedIsFractional.scala
@@ -0,0 +1,7 @@
+package sumito3478.math
+
+trait FixedIsFractional[Repr, @specialized(Byte, Short, Char, Int, Long) V] extends FixedIsNumeric[Repr, V] with Fractional[Repr]{
+  def div(x: Repr, y: Repr): Repr = {
+    valueOps.quot(valueOps.times(x, valueOps.fromInt(1 << base)), y)
+  }
+}
