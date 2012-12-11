4631d3a7fc6b1d0777edbbf22c98e6d7e442d326
sumito3478 <sumito3478@gmail.com>
2012-12-11 12:02:06 +0900

    Add dummy implicit vals to sumito3478

--- src/main/scala/sumito3478/package.scala
+++ src/main/scala/sumito3478/package.scala
@@ -1,3 +1,65 @@
 package object sumito3478 {
+  class Dummy0
 
+  implicit val dummy0 = new Dummy0
+
+  class Dummy1
+
+  implicit val dummy1 = new Dummy1
+
+  class Dummy2
+
+  implicit val dummy2 = new Dummy2
+
+  class Dummy3
+
+  implicit val dummy3 = new Dummy3
+
+  class Dummy4
+
+  implicit val dummy4 = new Dummy4
+
+  class Dummy5
+
+  implicit val dummy5 = new Dummy5
+
+  class Dummy6
+
+  implicit val dummy6 = new Dummy6
+
+  class Dummy7
+
+  implicit val dummy7 = new Dummy7
+
+  class Dummy8
+
+  implicit val dummy8 = new Dummy8
+
+  class Dummy9
+
+  implicit val dummy9 = new Dummy9
+
+  class Dummy10
+
+  implicit val dummy10 = new Dummy10
+
+  class Dummy11
+
+  implicit val dummy11 = new Dummy11
+
+  class Dummy12
+
+  implicit val dummy12 = new Dummy12
+
+  class Dummy13
+
+  implicit val dummy13 = new Dummy13
+
+  class Dummy14
+
+  implicit val dummy14 = new Dummy14
+
+  class Dummy15
+
+  implicit val dummy15 = new Dummy15
 }
