37c63aecdb0b47f45e7539fa64d755f83dd3b16b
sumito3478 <sumito3478@gmail.com>
2012-12-22 13:09:22 +0900

    Modify IndexedSeqTextLike to imp. CharSequence
    for #18.

--- src/main/scala/info/sumito3478/text/collection/IndexedSeqTextLike.scala
+++ src/main/scala/info/sumito3478/text/collection/IndexedSeqTextLike.scala
@@ -2,5 +2,14 @@ package info.sumito3478.text.collection
 
 import scala.collection.IndexedSeqOptimized
 
-trait IndexedSeqTextLike[+Repr] extends IndexedSeqOptimized[Char, Repr]{
+trait IndexedSeqTextLike[+Repr <: IndexedSeqTextLike[Repr]] extends IndexedSeqOptimized[Char, Repr] with CharSequence {
+  def charAt(idx: Int): Char = apply(idx)
+
+  def subSequence(start: Int, end: Int): CharSequence = {
+    slice(start, end)
+  }
+
+  override def toString(): String = {
+    (new scala.collection.mutable.StringBuilder ++= this).result
+  }
 }
