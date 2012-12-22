2062bf4a29efb058d4185494c7b67326215289d3
sumito3478 <sumito3478@gmail.com>
2012-12-22 13:42:34 +0900

    Add some documentation comments - for #18

--- src/main/scala/info/sumito3478/text/collection/IndexedSeqTextLike.scala
+++ src/main/scala/info/sumito3478/text/collection/IndexedSeqTextLike.scala
@@ -2,13 +2,25 @@ package info.sumito3478.text.collection
 
 import scala.collection.IndexedSeqOptimized
 
+/**
+ * A template trait for indexed sequences representing text data.
+ */
 trait IndexedSeqTextLike[+Repr <: IndexedSeqTextLike[Repr]] extends IndexedSeqOptimized[Char, Repr] with CharSequence {
+  /**
+   * implementation of CharSequence#charAt.
+   */
   def charAt(idx: Int): Char = apply(idx)
 
+  /**
+   * implementation of CharSequence#subSequence
+   */
   def subSequence(start: Int, end: Int): CharSequence = {
     slice(start, end)
   }
 
+  /**
+   * Convert text represented by this indexed sequence to java.lang.String.
+   */
   override def toString(): String = {
     (new scala.collection.mutable.StringBuilder ++= this).result
   }
--- src/main/scala/info/sumito3478/text/collection/immutable/StringText.scala
+++ src/main/scala/info/sumito3478/text/collection/immutable/StringText.scala
@@ -4,6 +4,9 @@ import info.sumito3478.text.collection.IndexedSeqTextLike
 import info.sumito3478.text.collection.mutable.StringTextBuilder
 import scala.collection.mutable.Builder
 
+/**
+ * A IndexedSeqTextLike implementation backed by java.lang.String.
+ */
 class StringText(val intern: String) extends IndexedSeq[Char] with IndexedSeqTextLike[StringText] {
   override def newBuilder: StringTextBuilder = new StringTextBuilder
   
